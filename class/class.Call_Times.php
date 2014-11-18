cd48356ee666957b1d59496f2bb8f4f9307e77a3
Samuel Melrose <sam.melrose@a1comms.com>
2014-11-18 16:33:23 +0000

    Initial Work on Call Time Query

--- class/class.Call_Times.php
+++ class/class.Call_Times.php
@@ -67,19 +67,47 @@ class Call_Times {
         return $this;
     }
 
-    public function getAVGWaitTime(){
-
-    }
-
     public function getAVGPausedTime(){
 
     }
 
-    public function getAVGTalkTime(){
+    public function getAVGHoldTime(){
 
     }
 
-    public function getAVGHoldTime(){
+    public function getAVGWaitTime(){
+        global $db;
+
+        $sql = "    SELECT
+                        AVG(wait_sec) as 'num'
+                    FROM
+                        vicidial_agent_log a
+                    LEFT OUTER JOIN
+                        vicidial_log b
+                            ON a.uniqueid = b.uniqueid
+                    LEFT OUTER JOIN
+                        vicidial_closer_log c
+                            ON a.uniqueid = c.uniqueid
+                    WHERE
+                        a.event_time > FROM_UNIXTIME('" . $db->escape_string($this->startEpoch) . "') AND a.event_time < FROM_UNIXTIME('" . $db->escape_string($this->endEpoch) . "')
+                    AND
+                        a.status IS NOT NULL
+                    " . ($this->campaign != "" ? " AND a.campaign_id = '" . $db->escape_string($this->campaign_id) . "'" : "" ) . "
+                    " . ($this->inorout != "all" ? ( " AND b.uniqueid IS " . ($this->inorout == "out" ? "NOT" : "") . "NULL") : "" ) . "
+                    " . ($this->queue != "" ? " AND c.campaign_id = '" . $db->escape_string($this->queue) . "'" : "" ) . "
+                    " . ($this->agent != "" ? " AND a.user = '" . $db->escape_string($this->agent) . "'" : "" ) . "
+                    " . ($this->dispo != "" ? " AND a.status = '" . $db->escape_string($this->dispo) . "'" : "" );
+
+        $result = $db->query($sql);
+        if ($result->num_rows == 1){
+            $row = $result->fetch_assoc();
+            return $row['num'];
+        } else {
+            return 0;
+        }
+    }
+
+    public function getAVGTalkTime(){
 
     }
 
