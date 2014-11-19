5e6edfeba150c1e3156d5e77ed9488b9ab3e314b
Samuel Melrose <sam.melrose@a1comms.com>
2014-11-19 10:01:29 +0000

    Add In Hold Times - Call Times

--- class/class.Call_Times.php
+++ class/class.Call_Times.php
@@ -99,7 +99,38 @@ class Call_Times {
     }
 
     public function getAVGHoldTime(){
+        global $db;
 
+        $sql = "    SELECT
+                        AVG(d.parked_sec) as 'num'
+                    FROM
+                        vicidial_agent_log a
+                    LEFT OUTER JOIN
+                        vicidial_log b
+                            ON a.uniqueid = b.uniqueid
+                    LEFT OUTER JOIN
+                        vicidial_closer_log c
+                            ON a.uniqueid = c.uniqueid
+                    JOIN
+                        parked_log d
+                            ON a.uniqueid = d.uniqueid
+                    WHERE
+                        a.event_time > FROM_UNIXTIME('" . $db->escape_string($this->startEpoch) . "') AND a.event_time < FROM_UNIXTIME('" . $db->escape_string($this->endEpoch) . "')
+                    AND
+                        a.status IS NOT NULL
+                    " . ($this->campaign != "" ? " AND a.campaign_id = '" . $db->escape_string($this->campaign) . "'" : "" ) . "
+                    " . ($this->inorout != "all" ? ( " AND b.uniqueid IS " . ($this->inorout == "out" ? "NOT" : "") . "NULL") : "" ) . "
+                    " . ($this->queue != "" ? " AND c.campaign_id = '" . $db->escape_string($this->queue) . "'" : "" ) . "
+                    " . ($this->agent != "" ? " AND a.user = '" . $db->escape_string($this->agent) . "'" : "" ) . "
+                    " . ($this->dispo != "" ? " AND a.status = '" . $db->escape_string($this->dispo) . "'" : "" );
+
+        $result = $db->query($sql);
+        if ($result->num_rows == 1){
+            $row = $result->fetch_assoc();
+            return $row['num'];
+        } else {
+            return 0;
+        }
     }
 
     public function getAVGWaitTime(){
@@ -324,7 +355,38 @@ class Call_Times {
     }
 
     public function getTotalHoldTime(){
+        global $db;
 
+        $sql = "    SELECT
+                        SUM(d.parked_sec) as 'num'
+                    FROM
+                        vicidial_agent_log a
+                    LEFT OUTER JOIN
+                        vicidial_log b
+                            ON a.uniqueid = b.uniqueid
+                    LEFT OUTER JOIN
+                        vicidial_closer_log c
+                            ON a.uniqueid = c.uniqueid
+                    JOIN
+                        parked_log d
+                            ON a.uniqueid = d.uniqueid
+                    WHERE
+                        a.event_time > FROM_UNIXTIME('" . $db->escape_string($this->startEpoch) . "') AND a.event_time < FROM_UNIXTIME('" . $db->escape_string($this->endEpoch) . "')
+                    AND
+                        a.status IS NOT NULL
+                    " . ($this->campaign != "" ? " AND a.campaign_id = '" . $db->escape_string($this->campaign) . "'" : "" ) . "
+                    " . ($this->inorout != "all" ? ( " AND b.uniqueid IS " . ($this->inorout == "out" ? "NOT" : "") . "NULL") : "" ) . "
+                    " . ($this->queue != "" ? " AND c.campaign_id = '" . $db->escape_string($this->queue) . "'" : "" ) . "
+                    " . ($this->agent != "" ? " AND a.user = '" . $db->escape_string($this->agent) . "'" : "" ) . "
+                    " . ($this->dispo != "" ? " AND a.status = '" . $db->escape_string($this->dispo) . "'" : "" );
+
+        $result = $db->query($sql);
+        if ($result->num_rows == 1){
+            $row = $result->fetch_assoc();
+            return $row['num'];
+        } else {
+            return 0;
+        }
     }
 
     public function getTotalWaitTime(){
