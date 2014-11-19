b04dd47eed4bcad19c8f957f3c302b80e168c8d7
Samuel Melrose <sam.melrose@a1comms.com>
2014-11-19 08:32:21 +0000

    Work For Yesterday

--- test/callTimes.php
+++ test/callTimes.php
@@ -6,6 +6,11 @@ $camp = (@$_GET['camp'] != "" ? $_GET['camp'] : 'CS');
 
 $data = new Campaign($camp);
 
+$yday_start = mktime(0,0,0) - (24*3600);
+$yday_end = mktime(23,59,59) - (24*3600);
+
+$data->setTimePeriod($yday_start, $yday_end);
+
 echo "<h1>Data For Campaign: " . $camp . "</h1>\n";
 
 print_r($data->byInbound()->fetchCallTimes()->byQueue('BUYM_CS')->byAgent('lrobinson')->byDispo('XFER2S')->getAVGWaitTime());
