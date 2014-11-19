848012ca23fa78cef05ebd966b1ebb2848650580
Samuel Melrose <sam.melrose@a1comms.com>
2014-11-19 08:28:21 +0000

    Remember to Initialize The Class

--- test/callTimes.php
+++ test/callTimes.php
@@ -2,4 +2,10 @@
 
 require "../config.php";
 
+$camp = (@$_GET['camp'] != "" ? $_GET['camp'] : 'CS');
+
+$data = new Campaign($camp);
+
+echo "<h1>Data For Campaign: " . $camp . "</h1>\n";
+
 print_r($data->byInbound()->fetchCallTimes()->byQueue('BUYM_CS')->byAgent('lrobinson')->byDispo('XFER2S')->getAVGWaitTime());
